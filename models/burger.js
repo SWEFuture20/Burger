742ffaf48943136c7d2ca9408c50830803860183
Latifah Lillie <queen1892@gmail.com>
2021-03-13 22:12:28 -0600

    Finished Adding the Routes in Controllers File

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+          cb(res);
+        });
+      },
+      create: function(name, cb) {
+        orm.create("burgers", name, function(res) {
+          cb(res);
+        });
+      },
+      update: function(condition, cb) {
+        orm.update("burgers", condition, function(res) {
+          cb(res);
+        });
+      },
 }
 module.exports = burger;
