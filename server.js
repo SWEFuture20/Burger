1bd4f383b4eddf354354960322cd1c63fa244f2e
Latifah Lillie <queen1892@gmail.com>
2021-03-14 19:10:16 -0500

    Fixing Heroku Issues Again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // var db = require("/models");
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
